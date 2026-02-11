99d294b2e0009c772f550b6a2ccb273adceed326
Louisemonte03 <louisemonte03@gmail.com>
2026-02-11 15:42:58 +0100

    Code review
    Alles omgezet naar het engels

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-// copy die we eerder hadden gebruikt
+// Copy we used earlier
 
 let base = "https://fdnd.directus.app/items";
 let endpoint =
   "/person?filter[squads][squad_id][tribe][name]=CMD%20Minor%20Web%20Dev&filter[squads][squad_id][cohort]=2526&sort=name&filter[fav_tag][_nempty]";
 let url = base + endpoint;
 
-const deLijst = document.querySelector(".student-list");
+const theList = document.querySelector(".student-list");
 const btn = document.querySelector("#random-btn");
 const display = document.querySelector("#student-display");
 
-let deMinorMensen = [];
+let minorPeople = [];
 
-async function getMinorMensen() {
+async function getMinorPeople() {
   let response = await fetch(url);
   let responseJSON = await response.json();
-  deMinorMensen = responseJSON.data;
+  minorPeople = responseJSON.data;
 
-  deMinorMensen.forEach((eenMinorMens) => {
-    let minorMensHTML = `
+  minorPeople.forEach((minorPerson) => {
+    let minorPersonHTML = `
             <li class="student-card">
-                <h4>${eenMinorMens.name}</h4>
-                <p><strong>${eenMinorMens.fav_emoji}</strong></p>
+                <h4>${minorPerson.name}</h4>
+                <p><strong>${minorPerson.fav_emoji}</strong></p>
             </li>`;
-    deLijst.insertAdjacentHTML("beforeend", minorMensHTML);
+    if (theList) {
+      theList.insertAdjacentHTML("beforeend", minorPersonHTML);
+    }
   });
 }
 
-// button voor de random student
+// Button for the random student
 btn.addEventListener("click", () => {
-  if (deMinorMensen.length > 0) {
+  if (minorPeople.length > 0) {
     let random =
-      deMinorMensen[Math.floor(Math.random() * deMinorMensen.length)];
+      minorPeople[Math.floor(Math.random() * minorPeople.length)];
     display.innerHTML = `
             <h4>${random.name}</h4>
             <p>${random.fav_emoji}</p>`;
   }
 });
 
-getMinorMensen();
+getMinorPeople();
 
-// light dark mode komt hier
+// Light/dark mode section
 const themeBtns = document.querySelectorAll(".theme-btn");
 
 themeBtns.forEach((btn) => {
